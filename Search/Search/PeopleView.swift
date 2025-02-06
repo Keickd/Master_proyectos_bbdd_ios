2130f99751e8f1639fe938a20d5136712092794c
Marcos Salas <keickd16@gmail.com>
2025-02-06 18:07:34 +0100

    eliminaciond e diplicados y busqueda de age

--- Search/Search/PeopleView.swift
+++ Search/Search/PeopleView.swift
@@ -12,11 +12,13 @@ struct PeopleView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var people: [Person]
     
-    init(criteria: String, sort: [SortDescriptor<Person>]) {
+    init(criteria: String, sort: [SortDescriptor<Person>], age: Int) {
         let predicate = #Predicate<Person> { person in
+            
             criteria.isEmpty ||
             person.name.localizedStandardContains(criteria) ||
-            person.lastname.localizedStandardContains(criteria)
+            person.lastname.localizedStandardContains(criteria) ||
+            person.age > age
         }
         
         _people = Query(filter: predicate, sort: sort)
@@ -31,6 +33,8 @@ struct PeopleView: View {
                             .font(.headline)
                         Text("\(person.address.street) \(person.address.zipCode)")
                             .font(.subheadline)
+                        Text("\(person.age)")
+                            .font(.footnote)
                     }
                 }
             }
@@ -41,11 +45,16 @@ struct PeopleView: View {
 struct ContentView: View {
     @State var criteria: String = ""
     @State var ascendingSort = true
+    
     @Environment(\.modelContext) private var modelContext
+    
+    var age: Int {
+        Int(criteria) ?? 0
+    }
     
     var body: some View {
         NavigationStack {
-            PeopleView(criteria: criteria, sort: sortDescriptors)
+            PeopleView(criteria: criteria, sort: sortDescriptors, age: age)
                 .toolbar {
                     ToolbarItem(placement: .topBarLeading) {
                         Button {
@@ -74,12 +83,17 @@ struct ContentView: View {
     
     private func load() {
         let people: [Person] = [
-            .init(name: "Michael", lastname: "Scoot", address: .init(street: "42 Dunkie Way", zipCode: 12345)),
-            .init(name: "John", lastname: "Williams", address: .init(street: "34 Faraday Street", zipCode: 456789))
+            .init(name: "Michael", lastname: "Scoot", address: .init(street: "42 Dunkie Way", zipCode: 12345), age: 56),
+            .init(name: "John", lastname: "Williams", address: .init(street: "34 Faraday Street", zipCode: 456789), age: 28)
         ]
         
         people.forEach { person in
             modelContext.insert(person)
+        }
+        do{
+            try modelContext.save()
+        }catch {
+            fatalError("")
         }
     }
 }
--- Search/Search/Person.swift
+++ Search/Search/Person.swift
@@ -9,14 +9,18 @@ import SwiftData
 
 @Model
 final class Person {
+    #Unique<Person>([\.name, \.lastname])
+    
     var name: String
     var lastname: String
     var address: Address
+    var age: Int
     
-    init(name: String, lastname: String, address: Address) {
+    init(name: String, lastname: String, address: Address, age: Int) {
         self.name = name
         self.lastname = lastname
         self.address = address
+        self.age = age
     }
 }
 
